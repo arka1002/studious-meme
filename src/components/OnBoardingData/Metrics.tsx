98e409e2f576ac002183bbe4dcd441b38d132f17
arka1002 <arka.mcu700@gmail.com>
2023-04-13 18:11:40 +0530

    now table

--- src/components/OnBoardingData/Metrics.tsx
+++ src/components/OnBoardingData/Metrics.tsx
@@ -18,15 +18,37 @@ export default function Metrics() {
                         <div className="text-5xl font-semibold green-gradient">88%</div>
                     </div>
                 </div>
-                <div><InputRange/></div>
+                <div><InputRange /></div>
                 <div><div className="font-semibold text-customGray-2000 text-center">Runaway</div>
                     <div className="flex justify-center mt-4">
-                    <div className="flex justify-center mt-4 gap-4">
-                        <div className="text-5xl font-semibold green-gradient">3.2</div>
-                        <div className="pt-2 font-semibold green-gradient">Months</div>
-                    </div>
+                        <div className="flex justify-center mt-4 gap-4">
+                            <div className="text-5xl font-semibold green-gradient">3.2</div>
+                            <div className="pt-2 font-semibold green-gradient">Months</div>
+                        </div>
                     </div></div>
             </div>
+            <div className="grid grid-cols-3 gap-10 mt-10">
+                <div className="p-5 bg-white rounded-lg shadow-lg">
+                    <div className="text-center font-semibold text-customGray-2000">CAC</div>
+                    <div className="mt-4 text-3xl font-semibold green-gradient">0</div>
+                </div>
+                <div className="p-5 bg-white rounded-lg shadow-lg">
+            
+                        <div className="text-center font-semibold text-customGray-2000">LTV</div>
+                        <div className="mt-4 text-3xl font-semibold green-gradient">$7.2</div>
+                    </div>
+    
+                <div className="p-5 bg-white rounded-lg shadow-lg">
+                
+                        <div className="text-center font-semibold text-customGray-2000">Churn Rate</div>
+                        <div className="flex justify-center mt-4 gap-4">
+                            <div className="text-5xl font-semibold green-gradient">3.2</div>
+                            <div className="pt-2 font-semibold green-gradient">Months</div>
+                        </div>
+                    </div>
+                
+            </div>
+            <div className="my-10 shadow-lg rounded-xl"></div>
         </div>
     )
 };
