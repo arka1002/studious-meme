547c19d24331d9cb2350f0fd87dfa697fbebde16
arka1002 <arka.mcu700@gmail.com>
2023-04-14 17:06:47 +0530

    done

--- src/components/OnBoardingData2/BusinessModelType.tsx
+++ src/components/OnBoardingData2/BusinessModelType.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 export default function BusinessModelType() {
     const items = [
         {
@@ -21,10 +24,28 @@ export default function BusinessModelType() {
             name: 'hardware financial model'
         }
     ]
+
+    const defaultBorder = 'border-2';
+    const activeStyle = 'border-[#1FC39E] border-2';
+    const [ active, setActive ] = useState(0);
+    function Change(params:any) {
+        console.log(params);
+        setActive(params)
+    }
+
     return (
         <div className="grid grid-cols-2 gap-y-8">
             {items.map(item => (
-                <div className="w-60 rounded-md border-2 bg-[#f3f3ff] py-3 px-5">{item.name}</div>
+                <>
+                {active == item.id ? (
+                    <div className={`w-60 rounded-md ${activeStyle} bg-[#f3f3ff] py-3 px-5`} onClick={() => Change(item.id)}>{item.name}</div>
+                ) : (
+                    <div className={`w-60 rounded-md ${defaultBorder} bg-[#f3f3ff] py-3 px-5`} onClick={() => Change(item.id)}>{item.name}</div>
+                )}
+                 
+                
+                </>
+                
             ))}
         </div>
     )
